4c2e635e5dbb3709d8277edefb969436ebfd601c
Jim Jones <jjones@aantix.com>
2011-11-18 17:56:29 -0800

    Updated settings to relect general gmail account settings.

--- config.rb
+++ config.rb
@@ -1,8 +1,12 @@
 SYMBOLS       = ['VTSMX','NAESX','VEIEX','VEURX','VPACX','VWEHX','VFSTX','VIPSX','VGSIX','VGPMX']
 
-# http://codelikezell.com/how-to-send-email-with-ruby-and-gmail/
-SMTP_OPTIONS  = {:server   => "example.com",
-                 :username => "username",
-                 :password => "password",
-                 :from     => "fund-anaylsis@localhost",
-                 :tls      => false}
+# Be sure to enable pop3 if you're going to be mailing with Gmail's SMTP server.
+# https://mail.google.com/support/bin/answer.py?answer=13273
+SMTP_OPTIONS  = {:address    => "smtp.gmail.com",
+                :domain      => 'gmail.com',
+                :username    => "your.gmailaccount@gmail.com",
+                :password    => "yourpassword",
+                :port        => 587,
+                :from        => "your.gmailaccount@gmail.com",
+                :subject     => "Fund Rebalance Report",
+                :tls         => true}
