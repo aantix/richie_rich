41a378d36cbeaaf5dc1870f3c927c0a6d6b84a9e
Jim Jones <jjones@aantix.com>
2011-11-23 01:08:41 -0800

    Fund symbols now have their allocations associated.  These allocations are now displayed on the email report.

--- config.rb
+++ config.rb
@@ -1,4 +1,13 @@
-SYMBOLS       = ['VTSMX','NAESX','VEIEX','VEURX','VPACX','VWEHX','VFSTX','VIPSX','VGSIX','VGPMX']
+SYMBOLS       = {'VTSMX' => 0.15,
+                 'NAESX' => 0.15,
+                 'VEIEX' => 0.1,
+                 'VEURX' => 0.1,
+                 'VPACX' => 0.1,
+                 'VWEHX' => 0.1,
+                 'VFSTX' => 0.1,
+                 'VIPSX' => 0.1,
+                 'VGSIX' => 0.05,
+                 'VGPMX' => 0.05}
 
 SMTP_OPTIONS  = {:address    => "smtp.gmail.com",
                 :domain      => 'gmail.com',
